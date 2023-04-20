50f97d2249036065eb9c7696eb0f1758c13c16f9
Patel Vishal <119497018+Vishu0617@users.noreply.github.com>
2023-04-20 15:46:59 +0530

    Commit success

--- src/AdminComponent/Login/AdminLogin.jsx
+++ src/AdminComponent/Login/AdminLogin.jsx
@@ -1,18 +1,16 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./AdminLogin.css";
-import video from '../../img/icon/cloud1.mp4'
-import {  toast } from 'react-toastify';
-import Button from '@mui/material/Button';
-
-
+// import video from '../../img/icon/cloud1.mp4'
+import { toast } from "react-toastify";
+import Button from "@mui/material/Button";
 
 function AdminLogin({ setAdminData }) {
   const navigate = useNavigate();
 
   const [adminLoginData, setAdminLoginData] = useState();
-  const [showPwd, setShowPwd] = useState('Password')
+  const [showPwd, setShowPwd] = useState("Password");
 
   // const [errorMessage, setErrorMessage] = useState()
 
@@ -31,7 +29,7 @@ function AdminLogin({ setAdminData }) {
       .then((res) => {
         // console.log(res.data.data);
         setAdminData(res.data.data);
-      const id=res.data.data._id;
+        const id = res.data.data._id;
         // console.log(res.data.token);
         // alert(res.data.message);
         toast.success(res.data.message, {
@@ -43,7 +41,7 @@ function AdminLogin({ setAdminData }) {
           draggable: true,
           progress: undefined,
           theme: "light",
-          });
+        });
         navigate(`/admin/AdminComponent/Dashbord/AdminDashbord/${id}`);
       })
       .catch((error) => {
@@ -58,64 +56,149 @@ function AdminLogin({ setAdminData }) {
           draggable: true,
           progress: undefined,
           theme: "light",
-          });
+        });
       });
   };
 
   return (
     <>
-    <div className="admin-login-body">
-    <video autoPlay muted loop src={video} className="login-video"></video>
-      <div className="rounded login-main shadow-lg container">
-       <h5 className="">Admin Login</h5><hr/>
-        <form className="row g-3 " onSubmit={loginSubmit}>
-          <div className="col-md-6 form-control admin-input border-dark  rounded  d-flex">
-            <label className="form-label mt-2">
-              <i className="zmdi zmdi-email"></i>
-            </label>
-            <input
-              className="inputsignup"
-              type="email"
-              placeholder="Enter E-mail"
-              name="email"
-              maxLength="255"
-              onChange={loginChack}
+        {/* <video autoPlay muted loop src={video} className="login-video"></video> */}
+      {/* <div className="admin-login-body">
+        <div className="rounded login-main shadow-lg container">
+          <h5 className="">Admin Login</h5>
+          <hr />
+          <form className="row g-3 " onSubmit={loginSubmit}>
+            <div className="col-md-6 form-control admin-input border-dark  rounded  d-flex">
+              <label className="form-label mt-2">
+                <i className="zmdi zmdi-email"></i>
+              </label>
+              <input
+                className="inputsignup"
+                type="email"
+                placeholder="Enter E-mail"
+                name="email"
+                maxLength="255"
+                onChange={loginChack}
 
-              // required
-            />
+                // required
+              />
+            </div>
+            <div className="col-md-6 form-control ms-0 admin-input  border-dark rounded  d-flex">
+              <label className="form-label mt-2">
+                <i className="zmdi zmdi-lock"></i>
+              </label>
+              <input
+                className="inputsignup"
+                type={showPwd}
+                placeholder="Enter Password"
+                name="pwd"
+                maxLength="10"
+                onChange={loginChack}
+                // required
+              />
+              {showPwd === "Password" ? (
+                <span className="icon-div" onClick={() => setShowPwd("text")}>
+                  <i className="zmdi zmdi-eye-off icon"></i>
+                </span>
+              ) : (
+                <span
+                  className="icon-div"
+                  onClick={() => setShowPwd("Password")}
+                >
+                  <i className="zmdi zmdi-eye icon"></i>
+                </span>
+              )}
+            </div>
+            <Link
+              className="link "
+              to="/AdminComponent/ForgetPassword/ForgetPassword"
+            >
+              Forgot Password
+            </Link>
+            <div>
+              <Button className="submit btn bg-dark text-white" type="Submit"
+                Submit
+              </Button>
+            </div>
+            {/* <div className="mt-2">
+          new?<Link className="link" align="right" to="/AdminComponent/SignUp/SignUp">Registration</Link></div> 
+          </form>
+        </div>
+      </div> */}
+
+      <div class="container col-xl-10 col-xxl-8 px-4 py-5">
+        <div class="row align-items-center g-lg-5 py-5">
+          <div class="col-lg-7 text-center text-lg-start">
+            <h1 class="display-4 fw-bold lh-1 mb-3">
+             wellcome to transport triangle
+            </h1>
+            <p class="col-lg-10 fs-4">
+             thisis the simple collage base project.
+            </p>
           </div>
-          <div className="col-md-6 form-control ms-0 admin-input  border-dark rounded  d-flex">
-            <label className="form-label mt-2">
-              <i className="zmdi zmdi-lock"></i>
-            </label>
-            <input
-              className="inputsignup"
-              type={showPwd}
-              placeholder="Enter Password"
-              name="pwd"
-              maxLength="10"
-              onChange={loginChack}
-              // required
-            />
-             {showPwd==="Password"?(
-               <span className="icon-div" onClick={()=>setShowPwd("text")}><i className="zmdi zmdi-eye-off icon"></i></span>
-            ):(
-              <span  className="icon-div" onClick={()=>setShowPwd("Password")}> <i className="zmdi zmdi-eye icon" ></i></span>
-            )} 
+          <div class="col-md-10 mx-auto col-lg-5">
+            <form
+              class="p-4 p-md-5 border rounded-3 bg-light"
+              onSubmit={loginSubmit}
+            >
+              <div className="col-md-6 form-control admin-input border-dark mt-2 rounded  d-flex">
+              <label className="form-label mt-2">
+                <i className="zmdi zmdi-email"></i>
+              </label>
+              <input
+                className="inputsignup"
+                type="email"
+                placeholder="Enter E-mail"
+                name="email"
+                maxLength="255"
+                onChange={loginChack}
+
+                // required
+              />
+            </div>
+              <div className="col-md-6 form-control ms-0 admin-input mt-2 border-dark rounded  d-flex">
+              <label className="form-label mt-2">
+                <i className="zmdi zmdi-lock"></i>
+              </label>
+              <input
+                className="inputsignup"
+                type={showPwd}
+                placeholder="Enter Password"
+                name="pwd"
+                maxLength="10"
+                onChange={loginChack}
+                // required
+              />
+              {showPwd === "Password" ? (
+                <span className="icon-div" onClick={() => setShowPwd("text")}>
+                  <i className="zmdi zmdi-eye-off icon"></i>
+                </span>
+              ) : (
+                <span
+                  className="icon-div"
+                  onClick={() => setShowPwd("Password")}
+                >
+                  <i className="zmdi zmdi-eye icon"></i>
+                </span>
+              )}
+            </div>
+              <div class="checkbox mb-3">
+                <label>
+                  <input type="checkbox" value="remember-me" /> Remember me
+                </label>
+              </div>
+              <Button class="w-100 btn btn-lg btn-primary" type="submit">
+                Sign up
+              </Button>
+              <hr class="my-4" />
+              {/* <small class="text-muted">
+                By clicking <Link className="link" align="right" to="/AdminComponent/SignUp/SignUp">Registration</Link>
+              </small> */}
+            </form>
           </div>
-          <Link className="link " to="/AdminComponent/ForgetPassword/ForgetPassword">
-          Forgot Password
-        </Link>
-          <div>
-            <Button className="submit btn bg-dark text-white" type="Submit"> Submit</Button>
-          </div>
-          {/* <div className="mt-2">
-          new?<Link className="link" align="right" to="/AdminComponent/SignUp/SignUp">Registration</Link></div> */}
-  
-        </form>
+        </div>
       </div>
-    </div>
-     </>
+    </>
   );
 }
 
